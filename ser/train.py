ade1e8d618bf6d0f5a618aa08cbfba590935ae79
CDTAdmin <cdtadmin@ndmpc967.ndm.ox.ac.uk>
2022-09-30 15:34:19 +0100

    split train/validate into seperate functions out of train_model in train.py

--- ser/train.py
+++ ser/train.py
@@ -1,16 +1,8 @@
 import torch
 import torch.nn.functional as F
 
-def train_model(validation_dataloader, training_dataloader, model, optimizer, epochs, device, name):
-    # train
-    with open('./experiments/model_'+name+'.txt', 'w') as f:
-                    f.write(f'Experiment file for model: {name} \n Model architecture: {model}')
-    best_acc = 0
-    best_acc_epoch = 0
-    best_model = None
-    best_batch = 0
-    for epoch in range(epochs):
-        for i, (images, labels) in enumerate(training_dataloader):
+def train(training_dataloader, device, model, optimizer, name, epoch):
+    for i, (images, labels) in enumerate(training_dataloader):
             images, labels = images.to(device), labels.to(device)
             model.train()
             optimizer.zero_grad()
@@ -27,37 +19,48 @@ def train_model(validation_dataloader, training_dataloader, model, optimizer, ep
             print(update_message)
             with open('./experiments/model_'+name+'.txt', 'a') as f:
                     f.write(update_message)
-        
 
-            # validate
-            val_loss = 0
-            correct = 0
-            with torch.no_grad():
-                for images, labels in validation_dataloader:
-                    images, labels = images.to(device), labels.to(device)
-                    model.eval()
-                    output = model(images)
-                    val_loss += F.nll_loss(output, labels, reduction="sum").item()
-                    pred = output.argmax(dim=1, keepdim=True)
-                    correct += pred.eq(labels.view_as(pred)).sum().item()
-                val_loss /= len(validation_dataloader.dataset)
-                val_acc = correct / len(validation_dataloader.dataset)
+def validate(validation_dataloader, device, model, epoch):
+    val_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for images, labels in validation_dataloader:
+            images, labels = images.to(device), labels.to(device)
+            model.eval()
+            output = model(images)
+            val_loss += F.nll_loss(output, labels, reduction="sum").item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(labels.view_as(pred)).sum().item()
 
-                print(
-                    f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
-                )
+    val_loss /= len(validation_dataloader.dataset)
+    val_acc = correct / len(validation_dataloader.dataset)
 
-                with open('./experiments/model_'+name+'.txt', 'a') as f:
-                    f.write(f"\n Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}")
+    print(f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}")
 
-                if val_acc > best_acc:
-                    best_acc = val_acc
-                    best_acc_epoch = epoch
-                    best_batch = i
-                    best_model = model
+    return val_loss, val_acc
+
+
+def train_model(validation_dataloader, training_dataloader, model, optimizer, epochs, device, name):
+    with open('./experiments/model_'+name+'.txt', 'w') as f:
+                    f.write(f'Experiment file for model: {name} \n Model architecture: {model}')
+    best_acc = 0
+    best_acc_epoch = 0
+    best_model = None
+
+    for epoch in range(epochs):
+        train(training_dataloader, device, model, optimizer, name, epoch)
+        val_loss, val_acc = validate(validation_dataloader, device, model, epoch)
+
+        with open('./experiments/model_'+name+'.txt', 'a') as f:
+            f.write(f"\n Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}")
+
+        if val_acc > best_acc:
+            best_acc = val_acc
+            best_acc_epoch = epoch
+            best_model = model
 
     with open('./experiments/model_'+name+'.txt', 'a') as f:
-                    f.write(f"\n\n Best validation accuracy: {best_acc} occured at epoch: {best_acc_epoch} and batch: {best_batch}") 
+                    f.write(f"\n\n Best validation accuracy: {best_acc} occured at epoch: {best_acc_epoch}") 
     # save model
     torch.save(best_model.state_dict(), './experiments/best_model_'+name) 
 
