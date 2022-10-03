b72da50efa30e2bbd7a40300aab4bfb92feebda6
CDTAdmin <cdtadmin@ndmpc967.ndm.ox.ac.uk>
2022-10-03 13:23:59 +0100

    Refactored code and added printed summary in model inference

---
+++ ser/evaluate.py
@@ -0,0 +1,30 @@
+import torch
+
+def evaluate(model, images):
+    model.eval()
+    output = model(images)
+    pred = output.argmax(dim=1, keepdim=True)[0].item()
+    certainty = max(list(torch.exp(output)[0]))
+    pixels = images[0][0]
+    print(_generate_ascii_art(pixels))
+    print(f"This is a {pred}")
+
+def _generate_ascii_art(pixels):
+    ascii_art = []
+    for row in pixels:
+        line = []
+        for pixel in row:
+            line.append(_pixel_to_char(pixel))
+        ascii_art.append("".join(line))
+    return "\n".join(ascii_art)
+
+
+def _pixel_to_char(pixel):
+    if pixel > 0.99:
+        return "O"
+    elif pixel > 0.9:
+        return "o"
+    elif pixel > 0:
+        return "."
+    else:
+        return " "
