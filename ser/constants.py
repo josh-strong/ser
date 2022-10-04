a338e76d3c625eef15886f403ad7649354f14a9c
CDTAdmin <cdtadmin@ndmpc967.ndm.ox.ac.uk>
2022-10-04 09:42:06 +0100

    Added code to test saving and loading parameters

--- ser/constants.py
+++ ser/constants.py
@@ -3,4 +3,3 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 RESULTS_DIR = PROJECT_ROOT / "results"
-TEST_DIR = PROJECT_ROOT / "tests"
--- tests/test_params.py
+++ tests/test_params.py
@@ -1,34 +1,16 @@
-from ser.params import Params, save_params
-import tempfile
-import shutil
-import json
 from pathlib import Path
-from ser.constants import TEST_DIR
+import tempfile
 
-test_params_input = Params("test", 5, 4000, 0.01, "abcdefgh")
-tmp_dirpath = tempfile.mkdtemp()
-save_params(TEST_DIR / tmp_dirpath, test_params_input)
+from ser.params import Params, save_params, load_params
 
 
-def test_save_params():
-    test_params_input = Params("test", 5, 4000, 0.01, "abcdefgh")
-    tmp_dirpath = tempfile.mkdtemp()
-    save_params(tmp_dirpath, test_params_input)
-
-    test_params_output = {
-        "name": "test",
-        "epochs": 1,
-        "batch_size": 4000,
-        "learning_rate": 0.01,
-        "commit": "28029962b990777e52155d858213cc4dcfc38932",
-    }
-
-    with open(tmp_dirpath + "output.json") as fp:
-        json.dump(test_params_output, fp)
-
-    a = json.load(tmp_dirpath + "/params.json")
-    b = json.load(tmp_dirpath + "/output.json")
-
-    assert sorted(a.items()) == sorted(b.items())
-
-    shutil.rmtree(tmp_dirpath)
+def test_save_and_load_params():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        run_path = Path(tmpdir)
+        input = Params("test", 1, 1, 1.0, "abcdefg")
+        # If we save the parameters to our temporary directory
+        save_params(run_path, input)
+        # And then load them again
+        output = load_params(run_path)
+        # We should find that what we loaded is equal to what we saved
+        assert input == output
